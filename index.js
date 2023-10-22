fd6d18f772dd369d0ae4a163595aeed921cc8c97
Merima Dedic <merimaxdedic@gmail.com>
2023-10-21 19:54:19 -0500

    Add index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) { // Assuming the maximum value for left is 360
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  // Call the function wherever you need it
+  moveDodgerRight();
